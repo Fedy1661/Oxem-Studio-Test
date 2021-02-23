5546e907f7ee89d30f386a2baa7b78ff51d213c2
Fedor Konovalov <fedykonovalov@gmail.com>
2021-02-23 11:41:55 +0300

    Rehab

--- main.php
+++ main.php
@@ -1,58 +1,59 @@
 <?php
-
-abstract class Animals
+class Farm
 {
-    public $amount;
-    public $animals = array();
+    private $animals = array();
 
     private $chars = "qazxswedcvfrtgbnhyujmkiolp1234567890QAZXSWEDCVFRTGBNHYUJMKIOLP";
     private $max_id = 6;
 
-    function __construct($amount)
+    public function add_animal($type, $amount_animals = 1, $range = null)
     {
-        $this->amount = $amount;
+        if (array_key_exists($type, $this->animals)) {
+            $ids = $this->animals[$type]['ids'];
+            $this->animals[$type]['ids'] =
+                array_merge($ids, $this->get_id($amount_animals));
+        } else {
+            $this->animals[$type] = [
+                'ids' => $this->get_id($amount_animals),
+                'range' => $range,
+                'collected' => 0
+            ];
+        }
+    }
+
+    public function collect_products()
+    {
+        $total_products = array();
+        foreach ($this->animals as $key => $value) {
+            $amount = count($value['ids']);
+            $products = $value['collected'];
+            $range = $value['range'];
+            for ($i = 0; $i < $amount; $i++) {
+                $products += rand($range[0], $range[1]);
+            }
+            $total_products[$key] = $products;
+            $this->animals[$key]['collected'] = $products;
+        }
+
+        return $total_products;
+    }
+
+    private function get_id($amount = 1)
+    {
+        $ids = [];
         for ($i = 0; $i < $amount; $i++) {
-            array_push($this->animals, $this->get_id());
+            $id = "";
+            for ($j = 0; $j < $this->max_id; $j++) {
+                $id .= $this->chars[rand(0, 61)];
+            }
+            array_push($ids, $id);
         }
-    }
-
-    private function get_id()
-    {
-        $password = "";
-        for ($i = 0; $i < $this->max_id; $i++) {
-            $password .= $this->chars[rand(0, 61)];
-        }
-        return $password;
+        return $ids;
     }
 }
 
-class Cows extends Animals
-{
-    public function get_milk()
-    {
-        $liters = 0;
-        for ($i = 0; $i < $this->amount; $i++) {
-            $liters += rand(8, 12);
-        }
-        return $liters;
-    }
-}
-
-class Chickens extends Animals
-{
-    public function get_eggs()
-    {
-        $eggs = 0;
-        for ($i = 0; $i < $this->amount; $i++) {
-            $eggs += rand(0, 1);
-        }
-        return $eggs;
-    }
-}
-
-
-$cows = new Cows(10);
-$chickens = new Chickens(20);
-$liters = $cows->get_milk();
-$eggs = $chickens->get_eggs();
-echo "Собрано {$liters} литров и {$eggs} яиц.";
+$farm = new Farm();
+$farm->add_animal('cow', 10, [8, 12]);
+$farm->add_animal('chicken', 20, [0, 1]);
+$products = $farm->collect_products();
+echo "Собрано {$products['cow']} литров молока и {$products['chicken']} яиц";
