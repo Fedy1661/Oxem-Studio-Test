1da0c2f633b40d05f07c2e971011aaf0998a3fc1
Fedor Konovalov <fedykonovalov@gmail.com>
2021-02-23 18:19:07 +0300

    Abstract animal and resource

--- main.php
+++ main.php
@@ -3,73 +3,83 @@ abstract class Animal
 {
     static $amount = 0;
 
-    abstract function __construct();
-    abstract function getProduct();
     public function getClass()
     {
         return get_class($this);
     }
+
+    abstract public function __construct($typeAnimal, $range, $typeOfResource);
+    abstract public function getProduct();
+    abstract public function getTypeOfAnimal();
+    abstract public function getTypeOfResource();
 }
 
-
-class Cow extends Animal
-{
-    function __construct()
-    {
-        $this->id = ++parent::$amount;
-    }
-    function getProduct()
-    {
-        return rand(8, 12);
-    }
-}
-
-class Chicken extends Animal
-{
-    function __construct()
-    {
-        $this->id = ++parent::$amount;
-    }
-    function getProduct()
-    {
-        return rand(0, 1);
-    }
-}
+// abstract class Recource
+// {
+//     // public $name;
+//     // public $range;
+//     function __construct()
+//     {
+//     }
+// }
 
 class Farm
 {
     private $animals = [];
-    public $milk = 0;
-    public $eggs = 0;
 
-    public function addCow($amount = 1)
+    public function addAnimal($typeAnimal, $amount = 1, $range, $typeOfResource)
     {
         for ($i = 0; $i < $amount; $i++) {
-            $this->animals[] = new Cow();
+            $this->animals[] = new class($typeAnimal, $range, $typeOfResource) extends Animal
+            {
+                public $range;
+
+                function __construct($typeAnimal, $range, $typeOfResource)
+                {
+                    $this->typeAnimal = $typeAnimal;
+                    $this->range = $range;
+                    $this->typeOfResource = $typeOfResource;
+                    $this->id = ++parent::$amount;
+                }
+                public function getProduct()
+                {
+                    return rand($this->range[0], $this->range[1]);
+                }
+                public function getTypeOfAnimal()
+                {
+                    return $this->typeAnimal;
+                }
+                public function getTypeOfResource()
+                {
+                    return $this->typeOfResource;
+                }
+            };
         }
     }
-    public function addChicken($amount = 1)
+
+    // public function addResource($name, $range){
+    //     $this->resources[] = new class extends Recource{
+
+    //     }
+    // }
+
+    public function collectResources()
     {
-        for ($i = 0; $i < $amount; $i++) {
-            $this->animals[] = new Chicken();
-        }
-    }
-    public function collectResources(){
+        $resources = [];
         foreach ($this->animals as $value) {
-            switch ($value->getClass()) {
-                case "Cow":
-                    $this->milk += $value->getProduct();
-                    break;
-                case "Chicken":
-                    $this->eggs += $value->getProduct();
-                    break;
+            if (array_key_exists($value->getTypeOfResource(), $resources)) {
+                $resources[$value->getTypeOfResource()] += $value->getProduct();
+            } else {
+                $resources[$value->getTypeOfResource()] = $value->getProduct();
             }
         }
+        return $resources;
     }
 }
 
 $farm = new Farm();
-$farm->addCow(10);
-$farm->addChicken(20);
-$farm->collectResources();
-echo "Собрано {$farm->milk} литров молока и {$farm->eggs} яиц";
+$farm->addAnimal('cow', 10, [8, 12], 'milk');
+$farm->addAnimal('chicken', 20, [0, 1], 'eggs');
+$products = $farm->collectResources();
+
+echo "Собрано {$products['milk']} литров молока и {$products['eggs']} яиц";
